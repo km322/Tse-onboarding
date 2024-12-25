c4e38f4903e1a80c11fa16381b913d519d67e989
km322 <mittal.ketan1@gmail.com>
2024-12-25 02:13:01 -0800

    part 1.2 implementation (check marks)

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Task, updateTask } from "src/api/tasks";
 import { CheckButton } from "src/components";
 import styles from "src/components/TaskItem.module.css";
-
-import type { Task } from "src/api/tasks";
 
 export interface TaskItemProps {
   task: Task;
 }
 
-export function TaskItem({ task }: TaskItemProps) {
+export function TaskItem({ task: initialTask }: TaskItemProps) {
+  const [task, setTask] = useState<Task>(initialTask);
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  const handleToggleCheck = () => {
+    // your code here
+    setLoading(true);
+    updateTask({ ...task, isChecked: !task.isChecked })
+      .then((result) => {
+        if (result.success) {
+          setTask(result.data);
+        } else {
+          alert(result.error);
+        }
+        setLoading(false);
+      })
+      .catch((reason) => alert(reason));
+  };
+
   let checkedStyle = styles.textContainer;
   if (task.isChecked) {
     checkedStyle += ` ${styles.checked}`;
@@ -16,7 +33,7 @@ export function TaskItem({ task }: TaskItemProps) {
   return (
     <div className={styles.item}>
       {/* render CheckButton here */}
-      <CheckButton checked={task.isChecked} />
+      <CheckButton checked={task.isChecked} onPress={handleToggleCheck} disabled={isLoading} />
       <div className={checkedStyle}>
         <span className={styles.title}>{task.title}</span>
         {task.description && <span className={styles.description}>{task.description}</span>}
